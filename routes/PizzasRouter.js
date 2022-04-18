106e63d6acfb35fa76e5a258f23771ee41db0dd5
Luís Afonso <acmluis94@gmail.com>
2022-04-18 13:27:13 -0300

    7 a 15

--- routes/PizzasRouter.js
+++ routes/PizzasRouter.js
@@ -1,5 +1,9 @@
 const express = require('express');
-
+const PizzasController = require('../controllers/PizzasController');
 const router = express.Router();
+
+router.get('/', PizzasController.listar);
+router.get('/pizzas/buscar', PizzasController.buscar)
+router.get('/pizzas/:id', PizzasController.mostrar)
 
 module.exports = router;
